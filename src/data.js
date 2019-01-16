dbbbb768f07729d73be49c8afd1ff663738ec752
Amin Pakseresht <amin.pakseresht@workjam.com>
2019-01-15 21:58:58 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,135 @@
+export const data = [
+  {
+    id: '39d8b07e-848f-4d91-8ab3-edbd82a66bbc',
+    title: 'China Foreign Affairs University',
+    address: '1875 Macpherson Circle',
+    creditCardType: 'jcb'
+  },
+  {
+    id: 'a065c0e2-5372-40a1-8a79-94a1007a4b08',
+    title:
+      'Evangelische Fachhochschule Ludwigshafen Hochschule für Sozial- und Gesundheitswesen',
+    address: '34 Dayton Circle',
+    creditCardType: 'jcb'
+  },
+  {
+    id: '5d9816ba-9cc0-40d3-a086-b03d1ead1f35',
+    title: 'Universidad Internacional de las Américas',
+    address: '4473 Independence Lane',
+    creditCardType: 'maestro'
+  },
+  {
+    id: 'da1947ae-244e-4cdf-8b01-28a7c333933f',
+    title: 'Semera University',
+    address: '5 Forest Street',
+    creditCardType: 'china-unionpay'
+  },
+  {
+    id: 'b334d9a3-3dab-4bc1-b69c-d1c46a3950f3',
+    title: 'University of Siena',
+    address: '5 Katie Park',
+    creditCardType: 'diners-club-enroute'
+  },
+  {
+    id: 'ef2ff3d3-e3e0-45c7-ba6f-e64de0e33a94',
+    title: 'University of Latvia',
+    address: '58 Continental Trail',
+    creditCardType: 'jcb'
+  },
+  {
+    id: '09a123cc-5aec-499e-84ec-bfbd2c8f9e88',
+    title: 'Belarussian State Academy of Music',
+    address: '7770 Susan Junction',
+    creditCardType: 'mastercard'
+  },
+  {
+    id: '375e189e-ebd6-46a2-a7a9-feb5f983b606',
+    title: 'Institute of Teachers Education, Rajang',
+    address: '2916 Cascade Court',
+    creditCardType: 'maestro'
+  },
+  {
+    id: 'a8a68c67-4c14-4f7a-8090-aa52183b1dd5',
+    title: 'American International University West Africa',
+    address: '9580 Hayes Court',
+    creditCardType: 'mastercard'
+  },
+  {
+    id: 'ab659324-4096-4a08-901e-3743b310a4b4',
+    title: 'Daugavpils University',
+    address: '2 Clemons Circle',
+    creditCardType: 'jcb'
+  },
+  {
+    id: '2ffd7c00-481c-47b8-bf03-d635651bb329',
+    title: 'John Wesley College',
+    address: '68 Hayes Lane',
+    creditCardType: 'mastercard'
+  },
+  {
+    id: '6a127db4-0c9e-467d-9aa5-9e3fa442a6af',
+    title: 'Trinity College of Vermont',
+    address: '64916 Blaine Pass',
+    creditCardType: 'diners-club-international'
+  },
+  {
+    id: '4d2319d9-e888-41f4-8f31-c8a98fa43ef7',
+    title: 'Brexgata University Academy',
+    address: '8692 Dorton Point',
+    creditCardType: 'maestro'
+  },
+  {
+    id: '58ddb867-d243-4705-8c10-827a34b40d73',
+    title: 'Pacific University',
+    address: '112 Schlimgen Junction',
+    creditCardType: 'visa-electron'
+  },
+  {
+    id: 'cd022d5f-a902-45e7-9d54-e1568b455154',
+    title: 'Free International University of Social Studies',
+    address: '046 Duke Circle',
+    creditCardType: 'jcb'
+  },
+  {
+    id: 'ff506bf9-70ba-47e3-98f5-f527cbde660d',
+    title: 'Red Sea University',
+    address: '6 Wayridge Court',
+    creditCardType: 'instapayment'
+  },
+  {
+    id: '5f56286b-3fe6-4883-8404-601d93f4ca39',
+    title: 'Oakland University',
+    address: '45 Brickson Park Plaza',
+    creditCardType: 'diners-club-enroute'
+  },
+  {
+    id: 'd7d095a1-fdf5-487e-8291-a19b1d511103',
+    title: 'Gaziantep University',
+    address: '601 Vahlen Plaza',
+    creditCardType: 'laser'
+  },
+  {
+    id: '872abe7e-ac86-4bea-969e-02ce8a468177',
+    title: 'California Institute of Technology',
+    address: '304 Hintze Drive',
+    creditCardType: 'jcb'
+  },
+  {
+    id: 'cb646562-b664-4564-b386-42c8d9ae67d5',
+    title: "Ecole Supérieure d'Electronique de l'Ouest",
+    address: '1966 Lawn Place',
+    creditCardType: 'jcb'
+  },
+  {
+    id: '1191ca5e-0571-4ad9-87e1-535dc0fb3d13',
+    title: 'Universidade Portucalense Infante D. Henrique',
+    address: '32235 Morningstar Drive',
+    creditCardType: 'visa'
+  },
+  {
+    id: '69c8f17c-d9cd-4dc0-baa8-5facccecd65d',
+    title: 'Universidad Tecnológica Equinoccial',
+    address: '0871 Sundown Way',
+    creditCardType: 'jcb'
+  }
+];
